b489bdac21f3454b198a482794f2049c2cc40970
PF-peinturefigurine <studiopeinturefigurine@gmail.com>
2024-11-27 19:00:15 +0100

    Update script.js
    Signed-off-by: PF-peinturefigurine <studiopeinturefigurine@gmail.com>

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Obtenir le bouton
 let mybutton = document.getElementById("scrollToTopBtn");
 
-// Afficher le bouton quand l'utilisateur a défilé de 20px vers le bas
+// Afficher le bouton quand l'utilisateur a défilé de 10px vers le bas
 window.onscroll = function() {
     if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
         mybutton.style.display = "block";

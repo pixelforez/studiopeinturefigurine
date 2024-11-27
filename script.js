0b8cf25113e8f0cc9dc32a14ed67f48d87723e89
PF-peinturefigurine <studiopeinturefigurine@gmail.com>
2024-11-27 18:50:11 +0100

    Create script.js
    Signed-off-by: PF-peinturefigurine <studiopeinturefigurine@gmail.com>

---
+++ script.js
@@ -0,0 +1,19 @@
+// Obtenir le bouton
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// Afficher le bouton quand l'utilisateur a défilé de 20px vers le bas
+window.onscroll = function() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+};
+
+// Fonction pour faire défiler la page jusqu'en haut
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+}
